5458a2bd1c4d19f33cfd502da3e0428807131643
Cedric <yacmov@gmail.com>
2024-05-24 02:28:05 +0800

    feat(index): mobile design v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      theme: {
+        textShadow: {
+          enabled: true,
+          default: '0px 3px rgba(0,0,0,0.25)' 
+        }
+      }, 
       colors: {
+        'pearlbush': '#eaddd7',
+        'spinodal': '#cadbed',
+        'classicrose': '#fbd0d9',
+        'berylgreen': '#d2e0b6',
       },
     },
   },
