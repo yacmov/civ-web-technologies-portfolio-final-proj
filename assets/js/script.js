6085b2a0da5ee7408287553645021766cfa92d5d
Cedric <yacmov@gmail.com>
2024-05-25 22:57:19 +0800

    feat(contact): test success message added

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,18 @@ const initApp = () => {
 }
 
 document.addEventListener('DOMContentLoaded', initApp);
+
+// Get the contact form and success section elements
+const contactForm = document.getElementById('contact-form');
+const contactMeSuccess = document.getElementById('contactme-success');
+
+// Add a submit event listener to the form
+contactForm.addEventListener('submit', (e) => {
+  // Prevent the default form submission behavior
+  e.preventDefault();
+
+  // Toggle the visibility of the success section
+  contactMeSuccess.classList.toggle('hidden');
+
+  contactForm.reset();
+});
